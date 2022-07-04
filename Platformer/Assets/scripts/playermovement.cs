be2a5fa602b50e8f850ac5131e9bb2a60b742c2d
Twanisbetter <108663500+Twanisbetter@users.noreply.github.com>
2022-07-04 19:15:28 +0200

    kirby

---
+++ Platformer/Assets/scripts/playermovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
